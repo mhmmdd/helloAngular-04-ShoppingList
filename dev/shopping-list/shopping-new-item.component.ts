0756cf5a78b8c68e8c14f71c4fd1d3ecf8e2e01f
mhmmdd <mhmmd.dd@gmail.com>
2016-07-03 13:44:21 +0300

    Fixes #3

--- dev/shopping-list/shopping-new-item.component.ts
+++ dev/shopping-list/shopping-new-item.component.ts
@@ -1,28 +1,47 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {ListItem} from "../list-item";
 import {ShoppingListService} from "./shopping-list.service";
+import {ControlGroup, FormBuilder, Validators, Control} from "angular2/common";
 
 @Component({
   selector: 'shopping-list-new-item',
   template: `
-    <div class="input">
-      <label for="item-name">Name</label>
-      <input type="text" name="" id="item-name" [(ngModel)]="item.name">
-    </div>
-    <div class="input">
-      <label for="item-amount">Amount</label>
-      <input type="text" name="" id="item-amoun" [(ngModel)]="item.amount">
-    </div>
-    <button (click)="onClick()">Add Item</button>
+    <form [ngFormModel]="myForm" (ngSubmit)="onSubmit()">
+      <div class="input">
+        <label for="item-name">Name</label>
+        <input type="text" name="" id="item-name" [(ngModel)]="item.name" [ngFormControl]="myForm.controls['itemName']">
+      </div>
+      <div class="input">
+        <label for="item-amount">Amount</label>
+        <input type="text" name="" id="item-amoun" [(ngModel)]="item.amount" [ngFormControl]="myForm.controls['itemAmount']">
+      </div>
+      <button type="submit" [disabled]="!myForm.valid">Add Item</button>
+    </form>
   `
 })
 
-export class ShoppingListNewItemComponent {
+export class ShoppingListNewItemComponent implements OnInit{
   item = {name: '', amount: 0};
+  myForm: ControlGroup;
 
-  constructor(private _shoppingListService: ShoppingListService) {}
+  constructor(private _shoppingListService: ShoppingListService, private _formBuilder: FormBuilder) {}
 
-  onClick() {
+  onSubmit() {
     this._shoppingListService.insertItem({name: this.item.name, amount: this.item.amount});
+  }
+
+  ngOnInit():any {
+    this.myForm = this._formBuilder.group({
+      'itemName': ['', Validators.required],
+      'itemAmount': ['', Validators.compose([
+        Validators.required, greaterZero
+      ])]
+    });
+  }
+}
+
+function greaterZero(control: Control): {[s: string]: boolean} {
+  if(control.value <= 0) {
+    return {notEnough: true};
   }
 }
